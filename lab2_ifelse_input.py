103687a0f3d779bd3ec85e07399b571d0bc16a5f
e1ee1e11 <e1ee1e11e1ee@gmail.com>
2014-07-26 17:37:41 +0800

    parctice %s % variable

--- lab2_ifelse_input.py
+++ lab2_ifelse_input.py
@@ -3,8 +3,8 @@ print "How old are you?"
 age = input()
 
 if age>= 18:
-	print "You're an adult."
+	print "You're an %s-year-old adult." % age
 elif age<18 and age>=0: 
-	print "Go back to school."
+	print "%s-year-old child should Go back to school." % age
 else:
 	print "Be serious!"
