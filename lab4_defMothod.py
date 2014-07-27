81817fae7447c1c8e4cd8a0f0830a5c03a67e5b0
e1ee1e11 <e1ee1e11e1ee@gmail.com>
2014-07-27 18:45:47 +0800

    Complete a BMI program by using def func.

---
+++ lab4_defMothod.py
@@ -0,0 +1,26 @@
+# -*- coding: utf8 -*-
+
+def BMI(cm, kg):
+	cm = round(float(cm/100), 2) #round(a, 2) 將a四捨五入到小數點第2位
+	cm = round(float(cm*cm), 2)  #float(x) 將int x 變成 float x
+	n = round(float(kg/cm), 2)
+	return n
+
+print "Input your height. (cm)"
+cm = round(input()+0.00, 2)
+print "cm=%s"%cm
+print "Input your weight. (kg)"
+kg = round(input()+0.00, 2)
+print "kg=%s"%kg
+n = BMI(cm, kg)
+print "%s" %  n
+if cm > 0 and kg > 0 :
+	print "Your BMI is %s" % n
+	if n >= 25 :
+		print "FAT guy."
+	elif n < 20 :
+		print "Thin guy."
+	else :
+		print "fantastic! you're super model."
+else:
+	print "Be serious!" 
