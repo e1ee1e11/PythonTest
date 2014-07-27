48d72513d98576ee115825b2a393c1e4f60f5a96
e1ee1e11 <e1ee1e11e1ee@gmail.com>
2014-07-27 18:56:46 +0800

    modify BMI program .So that it can calculate BMI by BMI module

--- lab4_defMothod.py
+++ lab4_defMothod.py
@@ -1,10 +1,12 @@
 # -*- coding: utf8 -*-
-
+"""
 def BMI(cm, kg):
 	cm = round(float(cm/100), 2) #round(a, 2) 將a四捨五入到小數點第2位
 	cm = round(float(cm*cm), 2)  #float(x) 將int x 變成 float x
 	n = round(float(kg/cm), 2)
 	return n
+"""
+import BMI_module
 
 print "Input your height. (cm)"
 cm = round(input()+0.00, 2)
@@ -12,7 +14,7 @@ print "cm=%s"%cm
 print "Input your weight. (kg)"
 kg = round(input()+0.00, 2)
 print "kg=%s"%kg
-n = BMI(cm, kg)
+n = BMI_module.BMI(cm, kg)
 print "%s" %  n
 if cm > 0 and kg > 0 :
 	print "Your BMI is %s" % n
