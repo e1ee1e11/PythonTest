b68152bb6319c1fa01c4711e3c82e4b586f433b8
e1ee1e11 <e1ee1e11e1ee@gmail.com>
2014-07-31 23:55:38 +0800

    modify my_tuple and create eddie author dictionary

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
-my_tuple = (1,2,3,'a',"c")
+my_tuple = (1,2,3,'a','c')
 
-print my_tuple
+eddie = {"first_name" : "eddie", "last_name" : "kao", "age" : 30}
+
+author = { "name" : "eddie", "profile" : {"age" : 30, "tel" : "0928123456"} }
+
+print "my_tuple is ",my_tuple
+
+
