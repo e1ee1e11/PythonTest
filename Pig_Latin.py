1da061efa1e2130f16d43aa6623ba67c18c3dfff
e1ee1e11 <e1ee1e11e1ee@gmail.com>
2015-01-17 18:52:52 +0800

    using print word[1:len(word)] can be much eaiser than using for loop

--- Pig_Latin.py
+++ Pig_Latin.py
@@ -5,14 +5,14 @@ def Pig_Latin():
     if word.isalpha():
         #print 'word=%s' % word
         #step3
-	new_word = ""
+	'''new_word = ""
         for i in range(1, len(word)):
             new_word = new_word + word[i]
-    
-        new_word = new_word + word[0] + "ay"
-
+        '''
+        new_word = word + word[0] + "ay"
+	
         #step4
-        print 'Your new word is "%s"' % new_word
+        print 'Your new word is "%s"' % new_word[1:len(new_word)]
     else:
         Pig_Latin()
 
