d7c519edd50386835d9bec2c532cc3d63eb51aab
e1ee1e11 <e1ee1e11e1ee@gmail.com>
2015-01-23 19:16:45 +0800

    test list about .index("str") & .insert(index, "item")

--- test_list.py
+++ test_list.py
@@ -2,5 +2,6 @@ color = ["red", "orange", "yellow", "green"]
 print color
 color.append("blue")
 print color
-
+color.insert(1, "white")
+print "red is index: " + str(color.index("red"))
 print color[1:len(color)-1]
