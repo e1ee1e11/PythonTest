0ca28728316ed8eec737e68f1809f9135b0cf8b4
e1ee1e11 <e1ee1e11e1ee@gmail.com>
2015-02-08 17:20:21 +0800

    string concatenation is so easy by using "+"

---
+++ str_concat.py
@@ -0,0 +1,10 @@
+n = ["Michael", "Lieberman"]
+# Add your function here
+def join_strings(words):
+    result = ""
+    for item in words:
+        #result.append(item)
+        result += item  
+    return result
+print join_strings(n)
+
