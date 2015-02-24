28ca19fc4b7c8549dd79468ebc111186b1ef22bc
e1ee1e11 <e1ee1e11e1ee@gmail.com>
2015-02-24 16:53:36 +0800

    using "print var1, var2 " is much simpler

--- loop_over_dictionary.py
+++ loop_over_dictionary.py
@@ -2,4 +2,5 @@ d = {'a': 'apple', 'b': 'berry', 'c': 'cherry'}
 
 for key in d:
     # Your code here!
-    print  "%s %s" % (key,  d[key])
+    #print  "%s %s" % (key,  d[key])
+    print key, d[key]
